d59759e0ae9a6015f0f7ac57457a909dfad47a66
VictorD <l21060670@chihuahua.tecnm.mx>
2025-10-22 10:03:42 -0600

    Rectificacion de lecturas adc

--- src/hal/adc.cpp
+++ src/hal/adc.cpp
@@ -5,31 +5,9 @@ void ADC_Init(int pin) {
     GPIO_Init(pin, INPUT);  // Configura el pin como entrada
 }
 
-// Lee el valor de un sensor y lo convierte a su unidad
+// Lee el valor de un potenciómetro y lo convierte a voltaje
 float ADC_Read(int pin) {
-    int valor = analogRead(pin);                  // Lee valor del ADC
-    float voltaje = (valor * VREF) / ADCMAX;     // Convierte a voltaje
-
-    if (pin == TEMP_PIN) {
-        return voltaje * 100.0;  // LM35: 10mV por °C
-    }
-
-    if (pin == HUM_PIN) {
-        float hum = (voltaje / VREF) * 100.0;    // Humedad %
-        if (hum < 0) hum = 0;
-        if (hum > 100) hum = 100;
-        return hum;
-    }
-
-    if (pin == LUZ_PIN) {
-        float volt_min = VREF_LUX; // medir con luz máxima
-        float volt_max = 0.2; // medir con LDR cubierta
-        float lux = (volt_min - voltaje) / (volt_min - volt_max) * 100.0;
-
-        if (lux < 0) lux = 0;
-        if (lux > 100) lux = 100;
-        return lux;
-    }
-
-    return 0.0; // Si no es ningún pin válido
+    int valor = analogRead(pin);          // Lectura del ADC
+    float voltaje = (valor * VREF) / ADCMAX; // Conversión a volts (0–5 V o 0–3.3 V según placa)
+    return voltaje;
 }
